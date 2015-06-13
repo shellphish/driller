8ed1f4b805c72ea210360f039ef36d97797ba24c
Nick 'mpizza' Stephens <nick.stephens93@gmail.com>
2015-06-13 16:17:38 -0700

    Generate constraints in a nicer way and do a better job of tracking which branches are missed

--- drill.py
+++ drill.py
@@ -9,6 +9,14 @@ def ok(s):
     status = termcolor.colored("*", "cyan", attrs=["bold"])
     print "[%s] %s" % (status, s)
 
+def alert(s):
+    status = termcolor.colored("!", "yellow", attrs=["bold"])
+    print "[%s] %s" % (status, s)
+
+def warning(s):
+    status = termcolor.colored("-", "red", attrs=["bold"])
+    print "[%s] %s" % (status, s)
+
 def die(s):
     status = termcolor.colored("-", "red", attrs=["bold"])
     print "[%s] %s" % (status, s)
@@ -26,11 +34,13 @@ def branch_for_input(branch, inputstream):
 
     padded = inputstream.ljust(stdin_bytes_cnt, "\x00")
     pad_cnt = 0
-    constraints = [ ] 
+    constraints = [ stdin_bytes == branch.state.BVV(padded) ] 
+    '''
     for bit in range(0, stdin_bytes.length, 8)[::-1]:
         upper_bit = bit + 7
         constraints.append(stdin_bytes[upper_bit:bit] == ord(padded[pad_cnt]))
         pad_cnt += 1
+    '''
 
     return branch.state.se.satisfiable(extra_constraints = constraints)
 
@@ -66,15 +76,16 @@ def trace_branches(project, inputstream, fn):
         # if we've encountered a branch we mark it
         encountered[next_branch.addr] = fn
 
-        missed[next_branch.addr] = False
+        # remove branch we encountered from our missed branches dict
+        if next_branch.addr in missed:
+            del missed[next_branch.addr]
 
         # if we just missed we check to see if another branch has encountered it
-        if missed.get(missed_branch.addr) == None:
-            # it appears no one has touched this branch we'll let every one know it's
-            # in sight for us
-            missed[missed_branch.addr] = fn
+        if missed_branch.addr not in encountered:
+            # if not, let the system know we have this branch in our sights
+            missed[missed_branch.addr] = (missed_branch, fn)
 
-        ok(next_branch)
+        #ok(next_branch)
         branches = windup_to_branch(next_branch)
 
     return (taken, not_taken)
@@ -96,27 +107,57 @@ def windup_to_branch(path):
     return branches
 
 def main(argc, argv):
-    if (argc != 3):
-        print "usage: %s <binary> <inputdir>" % (argv[0])
+    if (argc != 4):
+        print "usage: %s <binary> <inputdir> <outputdir>" % (argv[0])
         return 1
 
     binary = argv[1]
     inputdir = argv[2]
+    outputdir = argv[3]
 
     ok("drilling into \"%s\" with inputs in \"%s\"" % (binary, inputdir)) 
     if os.path.isdir(inputdir):
         inputs = os.listdir(inputdir)
     else:
-        die("no directory \"%s\" found" % inputfile)
+        die("no directory \"%s\" found" % inputdir)
+
+    try:
+        os.makedirs(outputdir)
+    except OSError:
+        if not os.path.isdir(outputdir):
+            die("cannot make output directory \"%s\"" % outputdir)
+
 
     project = angr.Project(binary)
 
     for inputfile in inputs:
         path = os.path.join(inputdir, inputfile)
-        print trace_branches(project, open(path).read(), inputfile)
+        trace_branches(project, open(path).read(), inputfile)
 
-    print map(hex, filter(lambda v: missed[v], missed.keys()))
 
+    # now that we've found some branches which our fuzzer missed, let's drill into them
+    alert("found %d basic blocks are fuzzer had trouble reaching, drilling into them!" % len(missed))
+
+    # here we would get fuzzer stats to figure out which input id these new inputs should
+    # be
+    file_id = 0
+
+    for missed_addr in missed:
+        angr_path, input_file = missed[missed_addr]
+        if angr_path.state.satisfiable():
+            filename = "driller-%d" % file_id
+            outname = os.path.join(outputdir, filename)
+            fp = open(outname, "w")
+            fp.write(angr_path.state.posix.dumps(0))
+            fp.close()
+            ok("new input in %s!" % outname)
+            file_id += 1
+
+        else:
+            warning("path at %x is not satisfiable" % missed_addr)
+            continue
+
+    ok("drilled inputs created and place in %s" % outputdir)
 
 if __name__ == "__main__":
     sys.exit(main(len(sys.argv), sys.argv))
