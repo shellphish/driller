4b628895c3c4037ad07dc89a83d8ef7f448a976f
Nick 'mpizza' Stephens <nick.stephens93@gmail.com>
2015-06-25 21:30:10 -0700

    Remove SymbolicRead definition

--- driller/drill.py
+++ driller/drill.py
@@ -77,29 +77,6 @@ def dump_to_file(path):
 
     return outfile
 
-class SymbolicRead(simuvex.SimProcedure):
-    '''
-    A custom version of read which has a symbolic return value.
-    '''
-
-    def run(self, fd, dst, length):
-        self.argument_types = {0: SimTypeFd(),
-                               1: self.ty_ptr(SimTypeArray(SimTypeChar(), length)),
-                               2: SimTypeLength(self.state.arch)}
-        self.return_type = SimTypeLength(self.state.arch)
-
-        if self.state.se.max_int(length) == 0:
-            return self.state.se.BVV(0, self.state.arch.bits)
-
-        sym_length = self.state.se.BV("sym_length", self.state.arch.bits)
-        self.state.add_constraints(sym_length <= length)
-        self.state.add_constraints(sym_length >= 0)
-
-        _ = self.state.posix.pos(fd)
-        data = self.state.posix.read(fd, length)
-        self.state.store_mem(dst, data)
-        return sym_length
-
 def detect_arch(loader):
     '''
     :param loader: a CLE loader object to extract the architecture from
