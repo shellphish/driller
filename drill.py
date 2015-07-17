aaffd298e4361ae095271c2b867cefc32545025a
Nick Stephens <nick.d.stephens@gmail.com>
2015-07-17 16:01:07 -0700

    Reduce the number of arguments to drill.py

--- drill.py
+++ drill.py
@@ -32,13 +32,6 @@ def main(argv):
                         help="input directory", 
                         required=True)
 
-    parser.add_argument("-o", 
-                        dest="out_dir", 
-                        type=str, 
-                        metavar="<out_dir>", 
-                        help="output directory",
-                        required=True)
-
     parser.add_argument("-f", 
                         dest="fuzz_bitmap", 
                         type=str, 
@@ -53,13 +46,6 @@ def main(argv):
                         help="installation directory of driller's qemus",
                         required=True)
 
-    parser.add_argument("-j",
-                        dest="proc_cnt",
-                        type=int,
-                        metavar="<proc_cnt>",
-                        help="number of processes to invoke",
-                        default=1)
-
     parser.add_argument("-s",
                         dest="sync_dir",
                         type=str,
@@ -72,15 +58,13 @@ def main(argv):
     
     binary      = args.binary
     in_dir      = args.in_dir
-    out_dir     = args.out_dir
     fuzz_bitmap = args.fuzz_bitmap
     qemu_dir    = args.qemu_dir
-    proc_cnt    = args.proc_cnt
     sync_dir    = args.sync_dir
 
     for input_file in (d for d in os.listdir(in_dir) if not d.startswith('.')):
         input_data = open(os.path.join(in_dir, input_file), 'rb').read()
-        driller.tasks.drill.delay(binary, input_data, out_dir, open(fuzz_bitmap, 'rb').read(), qemu_dir)
+        driller.tasks.drill.delay(binary, input_data, open(fuzz_bitmap, 'rb').read(), qemu_dir)
 
     return 0
 
