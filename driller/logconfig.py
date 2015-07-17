9d602e81df86cd6fbbbdede623494fa1554056da
Nick Stephens <nick.d.stephens@gmail.com>
2015-07-17 16:28:18 -0700

    Update logconfig with more colors

--- driller/logconfig.py
+++ driller/logconfig.py
@@ -1,30 +1,34 @@
-
 import logging
-logging.basicConfig(format='%(levelname)-7s | %(asctime)-23s | %(name)-8s | %(message)s', level=logging.WARNING)
+logging.basicConfig(format='%(levelname)-1s | %(asctime)-23s | %(name)-8s | %(message)s', level=logging.WARNING)
 logging.getLogger("main").setLevel(logging.DEBUG)
 
+color_codes = (19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230)
 
-color_name = True
-color_msg = True
-original_emit = logging.StreamHandler.emit
-def emit_wrap(*args, **kwargs):
-        #import ipdb; ipdb.set_trace()
-        record = args[1]
-        color = hash(record.name) % 8 + 30
+hardcoded_codes = { 'example': 19 }
 
-        if color_name:
-                try:
-                        record.name = ("\x1b[%dm" % color) + record.name + "\x1b[0m"
-                except Exception:
-                        pass
+color_the_name = True
+color_the_msg = True
+_original_emit = logging.StreamHandler.emit
+def _emit_wrap(*args, **kwargs):
+    record = args[1]
 
-        if color_msg:
-                try:
-                        record.msg = ("\x1b[%dm" % color) + record.msg + "\x1b[0m"
-                except Exception:
-                        pass
+    if record.name in hardcoded_codes:
+        color = hardcoded_codes[record.name]
+    else:
+        color = color_codes[hash(record.name) % len(color_codes)]
 
-        original_emit(*args, **kwargs)
+    if color_the_name:
+        try:
+            record.name = ("\x1b[1;38;5;%dm" % color) + record.name + "\x1b[0m"
+        except Exception: #pylint:disable=broad-except
+                pass
 
-logging.StreamHandler.emit = emit_wrap
+    if color_the_msg:
+        try:
+            record.msg = ("\x1b[0;38;5;%dm" % color) + record.msg + "\x1b[0m"
+        except Exception: #pylint:disable=broad-except
+            pass
 
+    _original_emit(*args, **kwargs)
+
+logging.StreamHandler.emit = _emit_wrap
