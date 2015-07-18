df1617a7518f66d87470f948e057e4d7d7d8f026
Nick Stephens <nick.d.stephens@gmail.com>
2015-07-18 00:06:19 -0700

    Connect to Celery using config options

--- driller/tasks.py
+++ driller/tasks.py
@@ -2,9 +2,11 @@ import redis
 from celery import Celery
 
 from .driller import Driller
+import config
 
-app = Celery('tasks', broker='amqp://guest@localhost//', backend='redis://localhost')
-redis_pool = redis.ConnectionPool(host='localhost', port=6379, db=1)
+backend_url = "redis://%s:%d" % (config.REDIS_HOST, config.REDIS_PORT)
+app = Celery('tasks', broker=config.BROKER_URL, backend=backend_url)
+redis_pool = redis.ConnectionPool(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB)
 
 @app.task
 def drill(binary, input, fuzz_bitmap, qemu_dir):
