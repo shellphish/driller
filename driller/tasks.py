0097f33900b6d75df38b28012a1e09fb03e22326
Nick Stephens <nick.d.stephens@gmail.com>
2015-07-17 17:51:44 -0700

    Remove out_dir from the drill task's list of arguments

--- driller/tasks.py
+++ driller/tasks.py
@@ -7,7 +7,7 @@ app = Celery('tasks', broker='amqp://guest@localhost//', backend='redis://localh
 redis_pool = redis.ConnectionPool(host='localhost', port=6379, db=1)
 
 @app.task
-def drill(binary, input, out_dir, fuzz_bitmap, qemu_dir):
+def drill(binary, input, fuzz_bitmap, qemu_dir):
     redis_inst = redis.Redis(connection_pool=redis_pool)
 
     driller = Driller(binary, input, fuzz_bitmap, qemu_dir, redis=redis_inst)
