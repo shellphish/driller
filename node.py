591cacc1a78c0921546de9dd2a6315c81c02c8a2
Nick Stephens <nick.d.stephens@gmail.com>
2015-07-23 19:06:27 -0700

    Node spins up fuzzer worker with -Ofair to disable prefetching entirely and stop a static reserved task queue

--- node.py
+++ node.py
@@ -5,32 +5,44 @@ import sys
 import multiprocessing
 import driller.config as config
 
+import logging
+import logconfig
+
+l = logging.getLogger("driller.node")
+l.setLevel("INFO")
+
+
 def check_exec(d, p):
     path = os.path.join(d, p)
     return not os.path.isdir(path) and os.access(path, os.X_OK)
+
+def binary_dir_sane():
+    if not os.path.isdir(config.BINARY_DIR):
+        l.error("the binary directory specified in the config is not a directory")
+        return False
+
+    if not any(filter(lambda x: check_exec(config.BINARY_DIR, x), os.listdir(config.BINARY_DIR))):
+        l.error("no binary files detected in binary directory specified")
+        return False
+
+    return True
 
 def driller_node():
     n = multiprocessing.cpu_count()
 
     # verify that config.QEMU_DIR is sane
     if not os.path.isdir(config.QEMU_DIR):
-        print "the qemu directory specified in the config is not a directory"
+        l.error("the qemu directory specified in the config is not a directory")
         return 1
 
     if "driller-qemu-cgc" not in os.listdir(config.QEMU_DIR):
-        print "the qemu directory does not contain any file by the name of 'driller-qemu-cgc'"
+        l.error("the qemu directory does not contain any file by the name of 'driller-qemu-cgc'")
         return 1
 
-    # verify that config.BINARY_DIR contains some binaries
-    if not os.path.isdir(config.BINARY_DIR):
-        print "the binary directory specified in the config is not a directory"
+    if not binary_dir_sane():
         return 1
 
-    if not any(filter(lambda x: check_exec(config.BINARY_DIR, x), os.listdir(config.BINARY_DIR))):
-        print "no binary files detected in binary directory specified, failing fast"
-        return 1
-
-    print "[#] spinning up a driller node with %d workers" % n
+    l.info("spinning up a driller node with %d workers", n)
     args = ["celery", "-A", "driller.tasks", "worker", "-c", str(n), "-Q", "driller", "--loglevel=info"]
 
     os.execvp(args[0], args)
@@ -40,8 +52,11 @@ def fuzzer_node():
 
     n /= config.FUZZER_INSTANCES
 
-    print "[#] spinning up a fuzzer node with %d workers" % n
-    args = ["celery", "-A", "fuzzer.tasks", "worker", "-c", str(n), "-Q", "fuzzer", "--loglevel=info"]
+    if not binary_dir_sane():
+        return 1
+
+    l.info("spinning up a fuzzer node with %d workers", n)
+    args = ["celery", "-A", "fuzzer.tasks", "worker", "-c", str(n), "-Q", "fuzzer", "--loglevel=info", "-Ofair"]
 
     os.execvp(args[0], args)
 
