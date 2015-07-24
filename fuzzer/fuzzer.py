d69a7802a07994f6759895bdec5c9cee848c9448
Nick Stephens <nick.d.stephens@gmail.com>
2015-07-24 16:16:56 -0700

    Remove listen definition from fuzzer.py

--- fuzzer/fuzzer.py
+++ fuzzer/fuzzer.py
@@ -34,43 +34,6 @@ def hexescape(s):
 
     return ''.join(out)
 
-
-def listen(queue_dir, channel):
-    '''
-    listen for new inputs produced by driller
-
-    :param queue_dir: directory to places new inputs
-    :param channel: redis channel on which the new inputs will be arriving
-    '''
-
-    l.debug("subscring to redis channel %s" % channel)
-    l.debug("new inputs will be placed into %s" % queue_dir)
-
-    try:
-        os.makedirs(queue_dir)
-    except OSError:
-        l.warning("could not create output directory '%s'" % queue_dir)
-
-    redis_inst = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB)
-    p = redis_inst.pubsub()
-
-    p.subscribe(channel)
-
-    input_cnt = 0
-
-    for msg in p.listen():
-        if msg['type'] == 'message':
-            real_msg = pickle.loads(msg['data'])
-            out_filename = "driller-%d-%x-%x" % real_msg['meta']
-            l.debug("dumping new input to %s" % out_filename)
-            afl_name = "id:%06d,src:%s" % (input_cnt, out_filename)
-            out_file = os.path.join(queue_dir, afl_name)
-
-            with open(out_file, 'wb') as ofp:
-                ofp.write(real_msg['data'])
-
-            input_cnt += 1
-
 class Fuzzer(object):
     ''' Fuzzer object, spins up a fuzzing job on a binary '''
 
