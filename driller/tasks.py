5fde0bd16af194843964b2fde5ebaf7effdbe1a9
Nick Stephens <nick.d.stephens@gmail.com>
2015-08-15 22:22:56 -0700

    Make sure tasks.py accepts the correct number of arguments!

--- driller/tasks.py
+++ driller/tasks.py
@@ -16,11 +16,11 @@ app.conf.CELERY_ROUTES = config.CELERY_ROUTES
 redis_pool = redis.ConnectionPool(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB)
 
 @app.task
-def drill(binary, input, fuzz_bitmap):
+def drill(binary, input, fuzz_bitmap, tag):
     redis_inst = redis.Redis(connection_pool=redis_pool)
 
     binary_path = os.path.join(config.BINARY_DIR, binary)
-    driller = Driller(binary_path, input, fuzz_bitmap, redis=redis_inst)
+    driller = Driller(binary_path, input, fuzz_bitmap, tag, redis=redis_inst)
     try:
         return driller.drill()
     except Exception as e:
