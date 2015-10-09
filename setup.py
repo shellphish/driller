14a80baf1d19f604acf8fbe3aecf1420de0aa70c
Nick Stephens <nick.d.stephens@gmail.com>
2015-10-08 21:36:04 -0700

    Add a setup.py, project still mostly needs to be initialized from the command line

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+import subprocess
+
+TRACER_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/tracer.git#egg=tracer'
+FUZZER_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/fuzzer.git#egg=fuzzer'
+
+if subprocess.call(['pip', 'install', TRACER_URL]) != 0:
+    raise LibError("Unable to install tracer")
+
+if subprocess.call(['pip', 'install', FUZZER_URL]) != 0:
+    raise LibError("Unable to install fuzzer")
+
+setup(
+        name='driller',
+        version='1.0',
+        packages=['driller'],
+        install_requires=[
+            'cle',
+            'angr',
+            'redis',
+            'celery',
+            'simuvex',
+            'archinfo',
+            'dpkt-fix',
+            'termcolor',
+        ],
+)
