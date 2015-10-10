8800ccbae12ab55fa2244cfc3426e81ac0678a03
Nick Stephens <nick.d.stephens@gmail.com>
2015-10-09 18:08:21 -0700

    Install listen.py script in setup

--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ setup(
         name='driller',
         version='1.0',
         packages=['driller'],
+        data_files=[
+            ('bin/driller', ('bin/driller/listen.py',),),
+        ],
         install_requires=[
             'cle',
             'angr',
