f0c93ee47045864a09bc1da821f2f4750f23b452
Nick Stephens <nick.d.stephens@gmail.com>
2015-10-12 21:41:21 -0700

    Remove logconfig import from node and use correct task paths

--- node.py
+++ node.py
@@ -8,7 +8,6 @@ import nodeprofile
 import driller.config as config
 
 import logging
-import logconfig
 
 l = logging.getLogger("driller.node")
 l.setLevel("INFO")
@@ -30,15 +29,6 @@ def binary_dir_sane():
 
 def driller_node(n, outfile, errfile):
 
-    # verify that config.QEMU_DIR is sane
-    if not os.path.isdir(config.QEMU_DIR):
-        l.error("the qemu directory specified in the config is not a directory")
-        return 1
-
-    if "driller-qemu-cgc" not in os.listdir(config.QEMU_DIR):
-        l.error("the qemu directory does not contain any file by the name of 'driller-qemu-cgc'")
-        return 1
-
     if not binary_dir_sane():
         return 1
 
@@ -56,7 +46,7 @@ def fuzzer_node(n, outfile, errfile):
 
     l.info("spinning up a fuzzer node with %d workers", n)
 
-    args = ["celery", "-A", "fuzzer.tasks", "worker", "-c", str(n), "-Q", "fuzzer", "--loglevel=info", "-Ofair"]
+    args = ["celery", "-A", "driller.tasks", "worker", "-c", str(n), "-Q", "fuzzer", "--loglevel=info", "-Ofair"]
 
     with open(outfile, "w") as o:
         with open(errfile, "w") as e:
