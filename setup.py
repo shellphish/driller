f29a0787beb75dd2b8e45f7e68b1bd3b561d494a
Yan <yans@yancomm.net>
2015-10-13 22:31:47 -0700

    remove messy pip

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 import subprocess
 
-TRACER_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/tracer.git#egg=tracer'
-FUZZER_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/fuzzer.git#egg=fuzzer'
-
-if subprocess.call(['pip', 'install', TRACER_URL]) != 0:
-    raise LibError("Unable to install tracer")
-
-if subprocess.call(['pip', 'install', FUZZER_URL]) != 0:
-    raise LibError("Unable to install fuzzer")
+#TRACER_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/tracer.git#egg=tracer'
+#FUZZER_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/fuzzer.git#egg=fuzzer'
+#
+#if subprocess.call(['pip', 'install', TRACER_URL]) != 0:
+#   raise LibError("Unable to install tracer")
+#
+#if subprocess.call(['pip', 'install', FUZZER_URL]) != 0:
+#   raise LibError("Unable to install fuzzer")
 
 setup(
         name='driller',
