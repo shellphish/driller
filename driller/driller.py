b51b936d777a6b227121f2d613740deddc6f8085
tyb0807 <sontuan.vu119@gmail.com>
2017-11-13 10:03:47 +0100

    Cleanup driller code base

--- driller/driller.py
+++ driller/driller.py
@@ -31,34 +31,34 @@ class Driller(object):
         """
 
         self.binary      = binary
-        # redis channel identifier
+
+        # Redis channel identifier.
         self.identifier  = os.path.basename(binary)
         self.input       = input_str
-        self.fuzz_bitmap = fuzz_bitmap or "\xff"*65535
+        self.fuzz_bitmap = fuzz_bitmap
         self.tag         = tag
         self.redis       = redis
         self.argv = argv or [binary]
 
         self.base = os.path.join(os.path.dirname(__file__), "..")
 
-        # the simprocedures
+        # The simprocedures.
         self._hooks = {} if hooks is None else hooks
 
+        # The driller core, which is now an exploration technique in angr.
         self._core = None
 
-        # start time, set by drill method
-        self.start_time       = time.time()
+        # Start time, set by drill method.
+        self.start_time = time.time()
 
-        # set of all the generated inputs
-        self._generated       = set()
+        # Set of all the generated inputs.
+        self._generated = set()
 
-        # set the memory limit specified in the config
+        # Set the memory limit specified in the config.
         if config.MEM_LIMIT is not None:
             resource.setrlimit(resource.RLIMIT_AS, (config.MEM_LIMIT, config.MEM_LIMIT))
 
-        l.info("[%s] drilling started on %s", self.identifier, time.ctime(self.start_time))
-
-        self.fuzz_bitmap_size = len(self.fuzz_bitmap)
+        l.debug("[%s] drilling started on %s.", self.identifier, time.ctime(self.start_time))
 
 ### DRILLING
 
@@ -67,17 +67,17 @@ class Driller(object):
         Perform the drilling, finding more code coverage based off our existing input base.
         """
 
+        # Don't re-trace the same input.
         if self.redis and self.redis.sismember(self.identifier + '-traced', self.input):
-            # don't re-trace the same input
             return -1
 
-        # Write out debug info if desired
+        # Write out debug info if desired.
         if l.level == logging.DEBUG and config.DEBUG_DIR:
             self._write_debug_info()
         elif l.level == logging.DEBUG and not config.DEBUG_DIR:
             l.warning("Debug directory is not set. Will not log fuzzing bitmap.")
 
-        # update traced
+        # Update traced.
         if self.redis:
             self.redis.sadd(self.identifier + '-traced', self.input)
 
@@ -93,7 +93,7 @@ class Driller(object):
         A generator interface to the actual drilling.
         """
 
-        # set up alarm for timeouts
+        # Set up alarm for timeouts.
         if config.DRILL_TIMEOUT is not None:
             signal.alarm(config.DRILL_TIMEOUT)
 
@@ -114,25 +114,23 @@ class Driller(object):
         simgr = p.factory.simgr(s, save_unsat=True, hierarchy=False, save_unconstrained=r.crash_mode)
 
         t = angr.exploration_techniques.Tracer(trace=r.trace)
-        d = angr.exploration_techniques.DrillerCore(trace=r.trace)
         c = angr.exploration_techniques.CrashMonitor(trace=r.trace, crash_mode=r.crash_mode, crash_addr=r.crash_addr)
+        self._core = angr.exploration_techniques.DrillerCore(trace=r.trace)
 
         simgr.use_technique(c)
         simgr.use_technique(t)
         simgr.use_technique(angr.exploration_techniques.Oppologist())
-        simgr.use_technique(d)
+        simgr.use_technique(self._core)
 
         self._set_concretizations(simgr.one_active)
 
-        l.debug("drilling into %r", self.input)
-        l.debug("input is %r", self.input)
-
-        # used for finding the right index in the fuzz_bitmap
-        prev_loc = 0
+        l.debug("Drilling into %r.", self.input)
+        l.debug("Input is %r.", self.input)
 
         simgr.run()
 
         for state in simgr.diverted:
+            l.debug("Found a diverted state, explorting to some extent.")
             w = self._writeout(state.history.bbl_addrs[-1], state)
             if w is not None:
                 yield w
@@ -141,35 +139,35 @@ class Driller(object):
 
 ### EXPLORER
 
-    def _symbolic_explorer_stub(self, path):
-        # create a new path group and step it forward up to 1024 accumulated active paths or steps
-
+    def _symbolic_explorer_stub(self, state):
+        # Create a new simulation manager and step it forward up to 1024
+        # accumulated active states or steps.
         steps = 0
         accumulated = 1
 
         p = angr.Project(self.binary)
-        pg = p.factory.simgr(path, immutable=False, hierarchy=False)
+        simgr = p.factory.simgr(path, immutable=False, hierarchy=False)
 
-        l.info("[%s] started symbolic exploration at %s", self.identifier, time.ctime())
+        l.debug("[%s] started symbolic exploration at %s.", self.identifier, time.ctime())
 
-        while len(pg.active) and accumulated < 1024:
-            pg.step()
+        while len(simgr.active) and accumulated < 1024:
+            simgr.step()
             steps += 1
 
-            # dump all inputs
+            # Dump all inputs.
+            accumulated = steps * (len(simgr.active) + len(simgr.deadended))
 
-            accumulated = steps * (len(pg.active) + len(pg.deadended))
+        l.debug("[%s] stopped symbolic exploration at %s.", self.identifier, time.ctime())
 
-        l.info("[%s] symbolic exploration stopped at %s", self.identifier, time.ctime())
-
-        pg.stash(from_stash='deadended', to_stash='active')
-        for dumpable in pg.active:
+        for dumpable in simgr.deadended:
             try:
                 if dumpable.satisfiable():
                     w = self._writeout(dumpable.history.bbl_addrs[-1], dumpable)
                     if w is not None:
                         yield w
-            except IndexError: # if the path we're trying to dump wasn't actually satisfiable
+
+            # If the state we're trying to dump wasn't actually satisfiable.
+            except IndexError:
                 pass
 
 ### UTILS
@@ -179,22 +177,12 @@ class Driller(object):
         flag_vars = set()
         for b in state.cgc.flag_bytes:
             flag_vars.update(b.variables)
+
         state.unicorn.always_concretize.update(flag_vars)
-        # let's put conservative thresholds for now
+
+        # Let's put conservative thresholds for now.
         state.unicorn.concretization_threshold_memory = 50000
         state.unicorn.concretization_threshold_registers = 50000
-
-    @staticmethod
-    def _has_false(path):
-        # check if the path is unsat even if we remove preconstraints
-        claripy_false = path.se.false
-        if path.scratch.guard.cache_key == claripy_false.cache_key:
-            return True
-
-        for c in path.se.constraints:
-            if c.cache_key == claripy_false.cache_key:
-                return True
-        return False
 
     def _in_catalogue(self, length, prev_addr, next_addr):
         """
@@ -207,60 +195,65 @@ class Driller(object):
 
         :return: boolean describing whether or not the input generated is redundant.
         """
+
         key = '%x,%x,%x\n' % (length, prev_addr, next_addr)
 
         if self.redis:
             return self.redis.sismember(self.identifier + '-catalogue', key)
+
+        # No redis means no coordination, so no catalogue.
         else:
-            # no redis means no coordination, so no catalogue
             return False
 
     def _add_to_catalogue(self, length, prev_addr, next_addr):
         if self.redis:
             key = '%x,%x,%x\n' % (length, prev_addr, next_addr)
             self.redis.sadd(self.identifier + '-catalogue', key)
-        # no redis = no catalogue
+        # No redis = no catalogue.
 
-    def _writeout(self, prev_addr, path):
-        t_pos = path.posix.files[0].pos
-        path.posix.files[0].seek(0)
-        # read up to the length
-        generated = path.posix.read_from(0, t_pos)
-        generated = path.se.eval(generated, cast_to=str)
-        path.posix.files[0].seek(t_pos)
+    def _writeout(self, prev_addr, state):
+        t_pos = state.posix.files[0].pos
+        state.posix.files[0].seek(0)
 
-        key = (len(generated), prev_addr, path.addr)
+        # Read up to the length.
+        generated = state.posix.read_from(0, t_pos)
+        generated = state.se.eval(generated, cast_to=str)
+        state.posix.files[0].seek(t_pos)
 
-        # checks here to see if the generation is worth writing to disk
-        # if we generate too many inputs which are not really different we'll seriously slow down AFL
+        key = (len(generated), prev_addr, state.addr)
+
+        # Checks here to see if the generation is worth writing to disk.
+        # If we generate too many inputs which are not really different we'll seriously slow down AFL.
         if self._in_catalogue(*key):
-            self._core.encounters.remove((prev_addr, path.addr))
+            self._core.encounters.remove((prev_addr, state.addr))
             return
+
         else:
             self._add_to_catalogue(*key)
 
-        l.info("[%s] dumping input for %x -> %x", self.identifier, prev_addr, path.addr)
+        l.debug("[%s] dumping input for %#x -> %#x.", self.identifier, prev_addr, state.addr)
 
         self._generated.add((key, generated))
 
         if self.redis:
-            # publish it out in real-time so that inputs get there immediately
+            # Publish it out in real-time so that inputs get there immediately.
             channel = self.identifier + '-generated'
 
             self.redis.publish(channel, pickle.dumps({'meta': key, 'data': generated, "tag": self.tag}))
+
         else:
-            l.info("generated: %s", generated.encode('hex'))
+            l.debug("Generated: %s!", generated.encode('hex'))
 
         return (key, generated)
 
     def _write_debug_info(self):
         m = hashlib.md5()
         m.update(self.input)
-        f_name = os.path.join(config.DEBUG_DIR,
-                              self.identifier + '_' + m.hexdigest() + '.py')
+        f_name = os.path.join(config.DEBUG_DIR, self.identifier + '_' + m.hexdigest() + '.py')
+
         with open(f_name, 'w+') as f:
-            l.debug("Wrote debug log to %s", f_name)
-            f.write("binary = %r\n" % self.binary +
-                    "started = '%s'\n" % time.ctime(self.start_time) +
-                    "input = %r\n" % self.input +
-                    "fuzz_bitmap = %r" % self.fuzz_bitmap)
+            l.debug("Debug log written to %s.", f_name)
+            f.write("binary = %r\n" % self.binary
+                    + "started = '%s'\n" % time.ctime(self.start_time)
+                    + "input = %r\n" % self.input
+                    + "fuzz_bitmap = %r" % self.fuzz_bitmap)
