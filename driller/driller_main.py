e9d2b6d2627ab6352243bbb0d51cde3daa7323f5
Arvind <dnivra@users.noreply.github.com>
2021-03-30 18:58:17 -0700

    Unsat states must be treated as potential successors in Tracer

--- driller/driller_main.py
+++ driller/driller_main.py
@@ -125,7 +125,7 @@ class Driller(object):
 
         simgr = p.factory.simulation_manager(s, save_unsat=True, hierarchy=False, save_unconstrained=r.crash_mode)
 
-        t = angr.exploration_techniques.Tracer(trace=r.trace, crash_addr=r.crash_addr, copy_states=True)
+        t = angr.exploration_techniques.Tracer(trace=r.trace, crash_addr=r.crash_addr, copy_states=True, follow_unsat=True)
         self._core = angr.exploration_techniques.DrillerCore(trace=r.trace, fuzz_bitmap=self.fuzz_bitmap)
 
         simgr.use_technique(t)
